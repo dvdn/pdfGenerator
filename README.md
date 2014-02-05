fa27db9c655dfa68a8deaa0d9c596bbf1d617a08
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-02-05 02:33:20 +0100

    pdf generator v0.2 arguments can be passed to wkhtml cli

--- README.md
+++ README.md
@@ -33,19 +33,30 @@ Usage
 -------------
 
 call index.php and give in parameter the url to convert into pdf.
-
 Example (without vhost):
-http://localhost/pdfGenerator/web/index.php?url=www.example.org
+http://localhost/pdfGenerator/web/index.php?url=http://www.example.org
+You can also omit the scheme (AKA the protocol)
+http://localhost/pdfGenerator/web/index.php?url=google.fr
+If your url contains GET parameters, you need to encode it (using encodeURIComponent for instance) so it will look like this
+http://localhost/pdfGenerator/web/index.php?url=https%3A%2F%2Fwww.phpbb.com%2Fabout%2Ffeatures%2F%3Ffrom%3Dsubmenu
+Wkhtml has many parameters to refine the pdf generation as you can see here: http://madalgo.au.dk/~jakobt/wkhtmltoxdoc/wkhtmltopdf-0.9.9-doc.html
+All these parameters may be passed to the webservice:
+http://localhost/pdfGenerator/web/index.php?url=google.fr&orientation=landscape&zoom=2
 
 Remarks
 -------------
 
-By default there is no javascript to minimize resources consumption.
+By default there is no javascript to minimize resources consumption.You can re-enable it by setting disable-javascript to false.
 
 TODO
 -------------
 
 Securize this webservice using apache vhost, limit to a specific range of ip for instance.
+
+TODO?
+------------
+Check if the url exists and returns a valid http code?
+Add an option to enable javascript if needed?
 
 Contributing
 -------------
--- web/index.php
+++ web/index.php
@@ -12,15 +12,41 @@ if ($parsed_url != false){
     if (!isset($parsed_url['scheme'])){
         $url = 'http://' . $_GET['url'];
     }
+    
+    $snappy = new Pdf('xvfb-run -s \'-screen 0 1100x1024x16\' -a wkhtmltopdf');
+    
+    $snappy->setOption('lowquality', false);
+    $snappy->setOption('disable-javascript', true);
+    $snappy->setOption('disable-smart-shrinking', false);
+    check_get_params($snappy);
+    if (isset($_GET['margin']))
+    {
+        $snappy->setOption('margin-bottom', $_GET['margin']);
+        $snappy->setOption('margin-top', $_GET['margin']);
+        $snappy->setOption('margin-right', $_GET['margin']);
+        $snappy->setOption('margin-left', $_GET['margin']);
+    }
+    
     // Display the resulting pdf in the browser
     // by setting the Content-type header to pdf
+    // header('Content-Disposition: attachment; filename="file.pdf"');
     header('Content-Type: application/pdf');
-    header('Content-Disposition: attachment; filename="file.pdf"');
-    
-    $snappy = new Pdf('xvfb-run -a wkhtmltopdf');
-    $snappy->setOption('disable-javascript', true);
     echo $snappy->getOutput($url);
 }
 else{
     throw new Exception("$url is not a valid url.");
+}
+
+/*
+ * @function check if one the wkhtml params is present in the url and set if needed.
+ */
+function check_get_params($snappy)
+{
+    foreach($snappy->getOptions() as $option => $value)
+    {
+        if (isset($_GET[$option]))
+        {
+            $snappy->setOption($option, $_GET[$option]);
+        }
+    }
 }
