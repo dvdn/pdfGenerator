386d047466b6fe8b469b177d1dbbfc853735a5a9
Xavier Raffin <xavier.raffin@gmail.com>
2015-06-02 16:30:14 +0200

    Update comments
    Update comments

--- web/index.php
+++ web/index.php
@@ -33,10 +33,11 @@ if ($parsed_url != false){
     // header('Content-Disposition: attachment; filename="file.pdf"');
     header('Content-Type: application/pdf');
 
+    // Convert pdf in CMYK colorspace
+    // Need GhostScript
+    // Need a writeable temporary directory for php process
     if (isset($_GET['cmyk'])) {
-        // Convert pdf in CMYK colorspace
-        // Need GhostScript
-        // Need a writeable temporary directory for php process
+        
         $tmpRGBFileName = tempnam(sys_get_temp_dir(), 'pdf-rgb');
         $tmpCMYKFileName = tempnam(sys_get_temp_dir(), 'pdf-cmyk');
         
